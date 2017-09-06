07f2e83d94c3d349d39bb44695ea67ebb86f9ae8
chrism3027 <chris.m.mcclure@gmail.com>
2017-09-06 13:38:44 -0500

    Add project files.

---
+++ SpyStore_HOL.DAL/Repos/Interfaces/ICategoryRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SpyStore_HOL.DAL.Repos.Base;
+using SpyStore_HOL.Models.Entities;
+
+namespace SpyStore_HOL.DAL.Repos.Interfaces
+{
+    public interface ICategoryRepo : IRepo<Category>
+    {
+    }
+}
