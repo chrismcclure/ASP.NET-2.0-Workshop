7214951172fcf9db6753516a14ab0b4ee4568a93
chrism3027 <chris.m.mcclure@gmail.com>
2017-09-06 15:56:40 -0500

    added controllers

---
+++ SpyStore_HOL.MVC/ViewModels/CartRecordViewModel .cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SpyStore_HOL.MVC.Validation;
+using SpyStore_HOL.MVC.ViewModels.Base;
+
+namespace SpyStore_HOL.MVC.ViewModels
+{
+    public class CartRecordViewModel : CartViewModelBase
+    {
+        [MustNotBeGreaterThan(nameof(UnitsInStock))]
+        public int Quantity { get; set; }
+    }
+}
